73f941e760c6964e20163cc9a6e4137280e09f5d
Vitaliy Parkhomenko <parkhomenko.wd@gmail.com>
2022-01-25 20:44:28 +0200

    Added new fields

--- src/components/FormMenu.jsx
+++ src/components/FormMenu.jsx
@@ -4,10 +4,11 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from '@mui/material/ListItemButton';
 import ErrorOutline from "@mui/icons-material/ErrorOutline";
 import Box from "@mui/material/Box";
 
-const config = [
+export const config = [
   {
     names: [],
     group: 'information',
@@ -47,14 +48,13 @@ function FormMenu(props) {
       >
         <List>
           {config.map((item, index) => (
-            <ListItem key={index}>
-              <ListItemIcon>
-                {index === 0 && <ErrorOutline color="error" /* color="success" *//>}
-              </ListItemIcon>
-              <ListItemText>
-                {item.label}
-              </ListItemText>
-            </ListItem>
+            <FormMenuItem
+              key={index}
+              item={item}
+              index={index}
+              onChange={props.onChange}
+              active={props.group === item.group}
+            />
           ))}
         </List>
       </Drawer>
@@ -63,3 +63,23 @@ function FormMenu(props) {
 }
 
 export default FormMenu;
+
+function FormMenuItem(props) {
+  const handleChange = () => {
+    props.onChange(props.item.group);
+  };
+
+  return (
+    <ListItemButton
+      onClick={handleChange}
+      selected={props.active}
+    >
+      <ListItemIcon>
+        {props.index === 0 && <ErrorOutline color="error" /* color="success" *//>}
+      </ListItemIcon>
+      <ListItemText>
+        {props.item.label}
+      </ListItemText>
+    </ListItemButton>
+  );
+}
---
+++ src/components/FormWithTabs.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {Form} from 'react-final-form';
+import Box from '@mui/material/Box';
+import validate from '../validate';
+import FormMenu, {config} from './FormMenu';
+import FormContent from './FormContent';
+import FormLayout from './FormLayout';
+
+const drawerWidth = 240;
+
+function FormWithTabs() {
+  const [group, setGroup] = useState(config[0].group);
+  const changeGroup = (newGroup) => {
+    setGroup(newGroup);
+  };
+  const handleSubmit = (values) => {
+    console.log(values);
+  }
+
+  return (
+    <Form
+      onSubmit={handleSubmit}
+      component={FormLayout}
+      validate={validate}
+    >
+      <Box sx={{ display: 'flex' }}>
+        <FormMenu
+          drawerWidth={drawerWidth}
+          onChange={changeGroup}
+          group={group}
+        />
+        <FormContent
+          drawerWidth={drawerWidth}
+          group={group}
+        />
+      </Box>
+    </Form>
+  );
+}
+
+export default FormWithTabs;
